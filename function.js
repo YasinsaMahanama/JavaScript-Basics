f3361926ae0ba7088801d11bd6a5f3e6ae7fed47
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-05-27 10:03:41 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,54 @@
+function add(fn,sn){
+    return fn+sn;
+}
+
+console.log(add(10,8));
+
+
+function remainder(fn,sn){
+    return fn%sn;
+}
+
+console.log(remainder(10,4));
+
+
+function substract(fn,sn){
+    return fn-sn;
+}
+
+console.log(substract(10,4));
+
+
+function division(fn,sn){
+    return fn-sn;
+}
+
+console.log(division(10,4));
+
+
+function division(fn,sn){
+    return fn/sn;
+}
+
+console.log(division(10,4));
+
+
+function multiplication(fn,sn){
+    return fn*sn;
+}
+
+console.log(multiplication(10,4));
+
+
+function exponentiation(fn,sn){
+    return fn**sn;
+}
+
+console.log(exponentiation(10,4));
+
+
+function printCapital(city,country){
+    console.log(city + " is the capital of " + country);
+}
+printCapital("Sri Jayawardhanapura" ,"Sri Lanka");
+
