9e3181bd3c2dab8abcf52cc66bf1ea67130a45d9
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-05-27 09:17:33 +0530

    Create operation.js

---
+++ operation.js
@@ -0,0 +1,47 @@
+console.log(typeof(9));
+
+console.log(typeof("Chamara"));
+
+console.log(12+26);
+
+console.log("Kelaniya" + "University" + 2023);
+
+console.log("Kelaniya " + "University " + 2023);
+
+console.log(10%3)
+
+console.log(10 % 3);
+
+console.log(2**3);
+
+console.log(3**8);
+
+
+let a= 4;
+let b= 5;
+console.log(a % b);
+console.log(a**b);
+
+
+console.log(1 + 3 * 4 - 2);
+
+console.log((1 + 3) * (4 - 2));
+
+console.log(-(100 + 5));
+
+console.log("Hello," + "world!");
+
+console.log((-1) * (10) * (-20));
+
+console.log(2 * (4 + 5 * (6 + 3)));
+
+console.log(36 / 3 + 4 * 3);
+
+/*
+Precedence Order
+     ()
+     Unary +,-
+     **
+     *,/
+     +,-
+ */
