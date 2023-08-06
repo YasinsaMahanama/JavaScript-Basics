6b95220ef6d2cb6c95c5ed39621b14fb81dd79d4
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-06 13:56:02 +0530

    Create script.js

---
+++ For_loop/script.js
@@ -0,0 +1,13 @@
+var i;
+var product = 1;
+var numbers = prompt("Enter a max number");
+
+var num = parseInt(numbers);
+
+for(i=1;i<=num;i++)
+{
+    product = product * i;
+}
+
+alert("Factorial : " + product);
+console.log("Factorial : " + product);
