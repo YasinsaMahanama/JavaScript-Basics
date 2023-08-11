efa80fc27a40ddc0ba5d1c4ecf56479ea051df16
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-11 12:10:06 +0530

    Create script.js

---
+++ Operators_Arithmetic/script.js
@@ -0,0 +1,16 @@
+var n1 = 45, n2 = 10;
+
+document.write(n1 + n2, "<br/>");
+document.write(n1 - n2, "<br/>");
+document.write(n1 * n2, "<br/>");
+document.write(n1 / n2, "<br/>");
+document.write(n1 % n2, "<br/>");
+
+var result = n1 + n2 * n1;
+
+document.write(result, "<br/>");
+
+var result1 = (n1 + n2) * n1;
+
+document.write(result1, "<br/>");
+
