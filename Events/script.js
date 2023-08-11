eed20b6466b933e96ad7486cb88e3d303e4d6291
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-11 14:15:38 +0530

    Create script.js

---
+++ Events/script.js
@@ -0,0 +1,14 @@
+function MouseOver()
+{
+    document.images["img1"].src = "logo-1.png"
+}
+
+function MouseOut()
+{
+    document.images["img1"].src = "logo.png"
+}
+
+function Load()
+{
+    document.images["img1"].src = "logo.png"
+}
