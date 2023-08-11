a70697af8ed6e8dc111dad5ff8f4f5e3ddd4f14a
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-11 12:56:23 +0530

    Create script.js

---
+++ Operators_Comparison/script.js
@@ -0,0 +1,8 @@
+var num = 100;
+
+document.write(num == 10, "<br/>");
+document.write(num != 10, "<br/>");
+document.write(num >= 10, "<br/>");
+document.write(num <= 10, "<br/>");
+document.write(num > 10, "<br/>");
+document.write(num < 10, "<br/>");
